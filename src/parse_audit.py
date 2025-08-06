c61740fdc139c5c124574ea0aee20d7d3085d917
bugbat <85291787+bugbat@users.noreply.github.com>
2025-08-06 10:08:17 -0400

    remove unnecessary functions

--- src/parse_audit.py
+++ src/parse_audit.py
@@ -2,13 +2,6 @@ import pandas as pd
 from pathlib import Path
 import json
 
-def audit_to_df(audit=Path):
-    df = pd.read_excel('data/audits/test_audit.xlsx', sheet_name='Report Data 1', skiprows=2)
-    return df
-
-def create_csv(df=pd.DataFrame, output_path=Path):
-    df.to_csv(output_path)
-
 def parse_json(df):
     df['parsed'] =  df['Event Data'].apply(json.loads)
     df_parsed = pd.json_normalize(df['parsed'])
@@ -22,11 +15,15 @@ def main():
     # define folder for audits
     folder = Path('data/audits')
     output_path = Path('data/audits.csv')
+    df_parsed_list = []
 
     for file in folder.iterdir():
-        if file.is_file():
-            df = audit_to_df(file)
+        if file.is_file() and file.suffix == '.xlsx':
+            df = pd.read_excel(file, sheet_name='Report Data 1', skiprows=2)
             df_parsed = parse_json(df)
+            df_parsed_list.append(df_parsed)
+    df = pd.concat(df_parsed_list, ignore_index=True)
+    df.to_csv(output_path)
     # load each audit in that folder
     # parse the json in each audit
     # export csv of file data / append to existing
